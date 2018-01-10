0aac980fd6f354d191f62fae3091d65bf605ae10
Kozma István <pisti_kozma@hotmail.com>
2018-01-10 11:30:11 +0100

    neszetájl

--- gol.js
+++ gol.js
@@ -1,9 +1,43 @@
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 context.strokeStyle = "grey";
+TILESIZE = 15;
+ROWNUM = canvas.width / TILESIZE
+gameMap = [];
 
-for (i = 0; i < 1000; i+= 15) {
-  for (j = 0; j < 1000; j+= 15) {
-    context.strokeRect(i, j, 15, 15);
+// for (x = 0; x < 600; x+= TILESIZE) {
+//   gameMap.push([]);
+//   for (y = 0; y < 600; y+= TILESIZE) {
+//     context.strokeRect(x, y, TILESIZE, TILESIZE);
+
+//   }
+// }
+
+class Tile {
+  constructor(x, y, fillColor) {
+    this.x = x;
+    this.y = y;
+    this.fillColor = fillColor
+  }
+
+  getCords() {
+    return {
+      x: this.x * TILESIZE,
+      y: this.y * TILESIZE
+    }
   }
 }
+
+function setup () {
+  for (x = 0; x < ROWNUM; x++) {
+    gameMap.push([])
+    for (y = 0; y < ROWNUM; y++) {
+      context.strokeRect(x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE);
+      gameMap[x][y] = new Tile(x, y, 'black')
+    }
+  }
+}
+
+setup();
+context.fillStyle = gameMap[0][1].fillColor;
+context.fillRect(gameMap[0][1].getCords().x, gameMap[0][1].getCords().y, TILESIZE, TILESIZE)
