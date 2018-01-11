a55b0e4f4863cb7b03ca48aca9fc0ae8ab14987e
BalazsBarna <balazsbarni@gmail.com>
2018-01-11 14:38:16 +0100

    style add

--- gol.js
+++ gol.js
@@ -5,7 +5,13 @@ var context = null
 var canvas = null
 const TILESIZE = 15;
 var ROWNUM = null
-const SPAWNERPOSITIONS = [8, 10, 20, 25, 30, 33]
+const SPAWNERPOSITIONS = [
+  {x: 5, y:5},
+  {x: 35, y:30},
+  {x: 5, y:30},
+  {x: 35, y:5},
+  {x: 20, y:20},
+]
 const SPAWNERS = []
 const gameMap = [];
 let snek1 = null
@@ -16,6 +22,8 @@ let loseCount = {
   player1: 0,
   player2: 0
 }
+let applePosition = null
+let draw = true
 
 
 //     ***************   TILE class START  ***************
@@ -40,11 +48,15 @@ class Tile {
     context.fillStyle = this.fillColor;
     context.fillRect(this.x * TILESIZE, this.y * TILESIZE, TILESIZE, TILESIZE);
   }
-
+  
   clearCell() {
-    this.occupied = 0
-    context.strokeStyle = 'grey';
-    context.strokeRect(this.x * TILESIZE, this.y * TILESIZE, TILESIZE, TILESIZE);
+    this.occupied = 0;
+    if (this.x === applePosition.x && this.y === applePosition.y) {
+      drawApple()
+    } else {
+      context.fillStyle = 'white';
+      context.fillRect(this.x * TILESIZE, this.y * TILESIZE, TILESIZE, TILESIZE);
+    }
   }
 
   occupyCell(what, color) {
@@ -77,10 +89,14 @@ class SpawnerTile extends Tile {
 
   clearCell() {
     this.occupied = 0
-    context.fillStyle = 'black';
-    context.fillRect(this.x * TILESIZE, this.y * TILESIZE, TILESIZE, TILESIZE);
+    if (this.x === applePosition.x && this.y === applePosition.y) {
+      drawApple()
+    } else {
+      context.fillStyle = 'purple';
+      context.fillRect(this.x * TILESIZE, this.y * TILESIZE, TILESIZE, TILESIZE);
+    }
   }
-
+  
   spawn(generator) {
     let randInt = Math.floor(Math.random() * Math.floor(2));
     if (randInt === 1) {
@@ -103,6 +119,7 @@ class Snek {
     this.newDirection = actionKeys[0]
     this.direction = actionKeys[0]
     this.move()
+    this.score = 0
   }
 
   move() {
@@ -129,12 +146,19 @@ class Snek {
     }
     this.validatePos(head)
     if (gameMap[head.x][head.y].occupied !== 0) {
-      gameOver(this.fillColor)
+      gameOver(this.fillColor, -1)
     }
     if (this.fillColor ==='red') {
       gameMap[head.x][head.y].occupyCell(2, this.fillColor)
     } else if (this.fillColor === 'green'){
       gameMap[head.x][head.y].occupyCell(3, this.fillColor)
+    }
+    if (head.x === applePosition.x && head.y === applePosition.y) {
+      this.score++
+      spawnApple()
+      if (this.score === 3) {
+        gameOver(this.fillColor, 1)
+      }
     }
   }
 
@@ -217,43 +241,43 @@ class Generators {
   }
 
   blinker(x, y) {
-    this.map[x][y].occupyCell(1, 'blue' );
-    this.map[x + 1][y].occupyCell(1, 'blue' );
-    this.map[x + 2][y].occupyCell(1, 'blue' );
+    this.map[x][y].occupyCell(1, 'black' );
+    this.map[x + 1][y].occupyCell(1, 'black' );
+    this.map[x + 2][y].occupyCell(1, 'black' );
   }
 
   glider(x, y) {
-    this.map[x][y].occupyCell(1, 'blue' );
-    this.map[x + 1][y].occupyCell(1, 'blue' );
-    this.map[x + 2][y].occupyCell(1, 'blue' );
-    this.map[x + 2][y + 1].occupyCell(1, 'blue' );
-    this.map[x + 1][y + 2].occupyCell(1, 'blue' );
+    this.map[x][y].occupyCell(1, 'black' );
+    this.map[x + 1][y].occupyCell(1, 'black' );
+    this.map[x + 2][y].occupyCell(1, 'black' );
+    this.map[x + 2][y + 1].occupyCell(1, 'black' );
+    this.map[x + 1][y + 2].occupyCell(1, 'black' );
   }
 
   glider2(x, y) {
-    this.map[x][y].occupyCell(1, 'blue' );
-    this.map[x - 1][y].occupyCell(1, 'blue' );
-    this.map[x - 2][y].occupyCell(1, 'blue' );
-    this.map[x - 2][y - 1].occupyCell(1, 'blue' );
-    this.map[x - 1][y - 2].occupyCell(1, 'blue' );
+    this.map[x][y].occupyCell(1, 'black' );
+    this.map[x - 1][y].occupyCell(1, 'black' );
+    this.map[x - 2][y].occupyCell(1, 'black' );
+    this.map[x - 2][y - 1].occupyCell(1, 'black' );
+    this.map[x - 1][y - 2].occupyCell(1, 'black' );
   }
 
   pentomino(x, y) {
-    this.map[x][y].occupyCell(1, 'blue' );
-    this.map[x - 1][y].occupyCell(1, 'blue' );
-    this.map[x][y - 1].occupyCell(1, 'blue' );
-    this.map[x][y + 1].occupyCell(1, 'blue' );
-    this.map[x + 1][y + 1].occupyCell(1, 'blue' );
+    this.map[x][y].occupyCell(1, 'black' );
+    this.map[x - 1][y].occupyCell(1, 'black' );
+    this.map[x][y - 1].occupyCell(1, 'black' );
+    this.map[x][y + 1].occupyCell(1, 'black' );
+    this.map[x + 1][y + 1].occupyCell(1, 'black' );
   }
 
   toad(x, y) {
-    this.map[x][y].occupyCell(1, 'blue' );
-    this.map[x][y + 1].occupyCell(1, 'blue' );
-    this.map[x + 1][y + 2].occupyCell(1, 'blue' );
-    this.map[x + 2][y - 1].occupyCell(1, 'blue' );
-    this.map[x + 3][y + 1].occupyCell(1, 'blue' );
-    this.map[x + 3][y].occupyCell(1, 'blue' );
-    this.map[x + 3][y + 1].occupyCell(1, 'blue' );
+    this.map[x][y].occupyCell(1, 'black' );
+    this.map[x][y + 1].occupyCell(1, 'black' );
+    this.map[x + 1][y + 2].occupyCell(1, 'black' );
+    this.map[x + 2][y - 1].occupyCell(1, 'black' );
+    this.map[x + 3][y + 1].occupyCell(1, 'black' );
+    this.map[x + 3][y].occupyCell(1, 'black' );
+    this.map[x + 3][y + 1].occupyCell(1, 'black' );
   }
 }
 
@@ -261,7 +285,7 @@ class Generators {
 
 
 function createCanvas() {
-  CANVASCONTAINER.innerHTML = '<canvas class="sol" width="600" height="600"></canvas>'
+  CANVASCONTAINER.innerHTML = '<canvas class="sol" width="600" height="600" style="border: 1px dashed black;"></canvas>'
   canvas = document.querySelector('canvas')
   context = canvas.getContext("2d");
   ROWNUM = canvas.width / TILESIZE
@@ -271,15 +295,16 @@ function drawMap(gameMap) {
   for (let x = 0; x < ROWNUM; x++) {
     gameMap.push([])
     for (let y = 0; y < ROWNUM; y++) {
-      if (SPAWNERPOSITIONS.includes(y)) {
-        if (x === y ) {
+      let spawn = false
+      SPAWNERPOSITIONS.forEach(function(spawner) {
+        if (x === spawner.x && y === spawner.y) {
           gameMap[x][y] = new SpawnerTile(x, y, 'white')
           SPAWNERS.push(gameMap[x][y])
-        } else {
-          gameMap[x][y] = new Tile(x, y, 'white')
+          spawn = true
         }
-      } else {
-        gameMap[x][y] = new Tile(x, y, 'white')
+      })
+      if (spawn === false) {
+        gameMap[x][y] = new Tile(x, y, 'white')        
       }
       gameMap[x][y].clearCell()
     }
@@ -289,17 +314,18 @@ function drawMap(gameMap) {
 
 function setup () {
   createCanvas()
+  applePosition = {
+    x: Math.floor(Math.random() * Math.floor(20)) + 10,
+    y: 10
+  };
   const generator = new Generators(gameMap);
   drawMap(gameMap);
-  snek1 = new Snek(0, 39, 'green', 'wasd')
-  snek2 = new Snek(39, 39, 'red', 'ijkl')
+  snek1 = new Snek(1, 39, 'green', 'wasd')
+  snek2 = new Snek(38, 39, 'red', 'ijkl')
   document.addEventListener('keypress', function(e) {
-    snek1.getDirection(e.key)
-    snek2.getDirection(e.key)
+    snek1.getDirection(e.key.toLowerCase())
+    snek2.getDirection(e.key.toLowerCase())
   })
-  initialLife(generator);
-  // generator.glider(25, 25)
-  // generator.glider2(10, 25)
   generator.pentomino(10, 30)
   generator.pentomino(30, 10)
   spawnInterval = setInterval(function() {
@@ -312,41 +338,22 @@ function setup () {
     snek1.move()
     snek2.move()
     redrawMap(gameMap)
-  }, 300)
+  }, 200)
 
 }
 
 function initialLife(generator) {
-  generator.toad(10, 29)
-  generator.toad(6, 20)
-  generator.toad(2, 35)
   generator.blinker(10, 35)
   generator.blinker(15, 35)
   generator.blinker(20, 35)
   generator.blinker(25, 35)
   generator.blinker(30, 35)
-  generator.blinker(2, 30)
-  generator.blinker(10, 30)
-  generator.blinker(15, 30)
-  generator.blinker(20, 30)
-  generator.toad(25, 30)
   generator.blinker(30, 30)
   generator.blinker(2, 20)
   generator.blinker(10, 20)
   generator.blinker(15, 20)
-  generator.blinker(20, 20)
-  generator.blinker(25, 20)
-  generator.blinker(25, 25)
-  generator.toad(30, 20)
-  generator.blinker(2, 10)
-  generator.toad(10, 10)
   generator.blinker(15, 10)
   generator.blinker(20, 10)
-  generator.blinker(25, 10)
-  generator.toad(30, 10)
-  generator.toad(2, 5)
-  generator.blinker(10, 5)
-  generator.blinker(15, 5)
   generator.blinker(20, 5)
   generator.blinker(25, 5)
   generator.blinker(30, 5)
@@ -359,21 +366,21 @@ function redrawMap (map) {
     row.forEach(function(col, y) {
       if (map[x][y].occupied === 2) {
         if (map[x][y].neighbourCount === 2) {
-          gameOver('red')
+          gameOver('red', -1)
         }
         map[x][y].fillCell()
       } else if (map[x][y].occupied === 3) {
-          if (map[x][y].neighbourCount === 2) {
-            gameOver('green')
-          }
-          map[x][y].fillCell()
+        if (map[x][y].neighbourCount === 2) {
+          gameOver('green', -1)
+        }
+        map[x][y].fillCell()
       }
       else if (map[x][y].neighbourCount < 2) {
         map[x][y].clearCell()
       } else if (map[x][y].neighbourCount > 3) {
         map[x][y].clearCell()
       } else if (map[x][y].occupied === 0 && map[x][y].neighbourCount === 3) {
-        map[x][y].occupyCell(1, 'blue' );
+        map[x][y].occupyCell(1, 'black' );
       } else if (map[x][y].occupied !== 0){
         map[x][y].fillCell();
       }
@@ -398,19 +405,34 @@ function countNeighbours(map) {
   })
 }
 
-function gameOver(color) {
+function gameOver(color, score) {
   canvas.remove()
   if (color === 'green') {
-    loseCount.player1--
+    loseCount.player1 += score
   } else {
-    loseCount.player2--
+    loseCount.player2 += score
   }
-  CANVASCONTAINER.innerHTML = '<h1>The ' + color + ' Snek is the worst Snek EVER!</h1>'
+  if (score === -1) {
+    CANVASCONTAINER.innerHTML = '<h1>The ' + color + ' Snek is the WORST Snek EVER!</h1>'
+  } else {
+    CANVASCONTAINER.innerHTML = '<h1>The ' + color + ' Snek is the BEST Snek EVER!</h1>'    
+  }
   CANVASCONTAINER.innerHTML += '<h1>Score of Shame:</h1>'
-  CANVASCONTAINER.innerHTML += "<h2 style='color: green'>Green: " + loseCount.player1 + "</h2>"
-  CANVASCONTAINER.innerHTML += "<h2 style='color: red'>Red: " + loseCount.player2 + "</h2>"
+  CANVASCONTAINER.innerHTML += "<h1 style='color: green'>Green: " + loseCount.player1 + "</h1>"
+  CANVASCONTAINER.innerHTML += "<h1 style='color: red'>Red: " + loseCount.player2 + "</h1>"
   CANVASCONTAINER.innerHTML += "<button onclick='setup()'>Play again</button>"
   clearInterval(spawnInterval)
   clearInterval(drawInterval)
 }
+
+function drawApple() {
+  context.fillStyle = 'yellow';
+  context.fillRect(applePosition.x * TILESIZE, applePosition.y * TILESIZE, TILESIZE, TILESIZE);
+}
+
+function spawnApple() {
+  applePosition.x = Math.floor(Math.random() * Math.floor(39));
+  applePosition.y = Math.floor(Math.random() * Math.floor(39));
+}
+
 setup();
